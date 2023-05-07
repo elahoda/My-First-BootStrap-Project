8919bef9d760194c03720dc31ac5b90515829ef9
Eric Lahoda <elahoda@gmail.com>
2023-05-07 13:05:35 -0400

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(function() {
     $(".carousel").carousel({interval:2000});
-    $("#carouselButton").click(function(){
+    $("#carouselButton").on("click", function(){
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
                 $(".carousel").carousel("pause");
                 $("#carouselButton").children("i").removeClass("fa-pause");
@@ -13,3 +13,12 @@ $(function() {
     });
 
 });
+$(function() {
+    $("#reserveButton").on("click", function() {
+        $('#reserveModal').modal('show'); 
+    });
+    $("#loginButton").on("click", function() {
+        $('#loginModal').modal('show');
+    });
+});
+
